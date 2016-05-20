54fdd48350280123687bfa3b6762d9d328fdd5e2
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-05-19 23:38:51 -0400

    code running, but little ibd found...

--- analyze_snps.py
+++ analyze_snps.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import itertools as it
 import argparse
+import pyutils.iter as iter
 import find_ibd_segment as fis
 
 def main(args):
@@ -27,36 +28,37 @@ def main(args):
     def analyze_snp(i):
         # find first typed snp after query snp
         snp_bp = data.col_property[i,2]
-        typed_i = np.where(typed_snps_bp > snp_bp)[0][0]
+        v = np.where(typed_snps_bp > snp_bp)[0]
+        if len(v) > 0:
+            typed_i = v[0]
+        else:
+            typed_i = len(typed_snps_indices)-1
+
         n1, n2 = np.where(X[:,i] == 1)[0]
+        if (X[n1,typed_snps_indices[typed_i]] - X[n2, typed_snps_indices[typed_i]])**2 == 4:
+            return 0, 0
 
         typed_il, typed_ir = fis.find_boundaries(
                 X[n1,typed_snps_indices],
                 X[n2,typed_snps_indices],
                 typed_i)
+        typed_ir -= 1
 
         il = typed_snps_indices[typed_il]
         ir = typed_snps_indices[typed_ir]
         cM = data.col_property[ir, 1] - \
                 data.col_property[il, 1]
         ibd = (np.mean(X[n1,il:ir] == X[n2,il:ir]) > 0.99)
-        print(X[n1,typed_snps_indices][typed_il-1:typed_ir+1][:10])
-        print(X[n2,typed_snps_indices][typed_il-1:typed_ir+1][:10])
-        print(X[n1,typed_snps_indices][typed_il-1:typed_ir+1][-10:])
-        print(X[n2,typed_snps_indices][typed_il-1:typed_ir+1][-10:])
-        # the lines above were to check that the ibs detection algorithm really worked
-        # they currently don't produce the right output, i.e., the boundaries should show
-        # disagreement. probably because we should be looking at the first *typed* snp
-        # of disagreement.
-        return cM, ibd
+        return cM, int(ibd)
 
-    for (i, snp) in it.izip(data.sid_to_index(seed_snps.index), seed_snps.index):
-        print(i, snp)
+    for (i, snp) in iter.show_progress(
+            it.izip(data.sid_to_index(seed_snps.index), seed_snps.index),
+            total=len(seed_snps)):
+            # total=10):
         seed_snps.ix[snp, ['ibs_length', 'ibd']] = analyze_snp(i)
-        if i > 100:
-            break
 
     print(seed_snps.iloc[:100])
+    seed_snps.to_csv(args.outfile, sep='\t')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -66,6 +68,7 @@ if __name__ == '__main__':
     parser.add_argument('--seq-err', type=float, default=0.01)
     parser.add_argument('--gen-err', type=float, default=0.01)
     parser.add_argument('--p-thresh', type=float, default=0.05)
+    parser.add_argument('--outfile', required=True)
     args = parser.parse_args()
 
     main(args)
